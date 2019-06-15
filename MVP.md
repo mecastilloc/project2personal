d6a0281c08388c60211295b8dc06a93b078ccdea
ManuCas <manucastle@hotmail.com>
2019-06-15 10:23:48 -0500

    update server.js to connect with heroku

---
+++ MVP.md
@@ -0,0 +1,27 @@
+# MVP
+
+This web app is intended to manage collaboration information within a workgroup. There are many layers in which that goal can be achieved. 
+The **MVP** on its entirety can be broken down as follows:
+
+1. **"Task-organizer"**
+On the first layer, at least some structure to keep the different tasks must be outlined.
+
+    1. *Team members.*  There must be a way to specify different users or workmembers for that workgroup.
+    1. *Projects.*  Different projects that workgroup can be working on must also be included.
+    1. *Tasks categories.*  Categories to englobe all tasks must be included to easily outline project areas.
+    1. *Tasks themselves.*  Enlisting the tasks with the above outlined structured is the ultimate goal for this first product.
+
+2. **"Task-organizer-detailer"**
+On the second layer, we can go deeper on detailing different aspects of each task.
+
+    2. *Task time length.* How long a task might take to be completed is something the workgroup should take in account when laying out the project program.
+    2. *Task executor(s).* People en charge of either one of the tasks must be common knowledge to the whole workgroup.
+    2. *Task status.* The task status is something the entire team should appreciate having at a hand.
+    2. *Task dependencies.* This is refered to the new tasks that came out of other tasks development and constitute troubles that hinder the parent completion.
+
+3. **"Task-organizer-easy_display"**
+The third layer should consist on developing intuitive showcasing of the whole information.
+
+    3. *Tasks filterable listing.* Just as simple as a complete task list will always come in handy. It should probably include all data hold for each task.
+    3. *Member page.* Each member should be able to see all the tasks that relate to him/herself in a simple, intiuitive and editable way.
+    3. *Infographs.* It is always interesting to see data in graph form to quickly identify potential issues at a glance. Graphs containing tasks assigned per user, per project, completed, yet to complete, etc, might be the difference between a fundamental "go to" app or a "should be interestign to use" app.
---
+++ models/schema.sql
@@ -0,0 +1,119 @@
+-- ================================== Database
+
+USE wycgc3g4apmhrsf3;
+
+
+-- ================================== Basic tables 
+
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(128) NOT NULL,
+    user_password VARCHAR(64) NOT NULL,
+    phone_number INT,
+    email VARCHAR (128),
+    is_admin BOOLEAN,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE projects (
+	id INT NOT NULL AUTO_INCREMENT,
+    project_name VARCHAR(64) NOT NULL,
+    description VARCHAR(256),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE categories (
+	id INT NOT NULL AUTO_INCREMENT,
+	category_name VARCHAR(64) NOT NULL,
+    description VARCHAR(256),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE chat_messages (
+    id INT NOT NULL AUTO_INCREMENT,
+    chat_message VARCHAR(1024) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE mail_messages (
+    id INT NOT NULL AUTO_INCREMENT,
+    mail_message VARCHAR(2048) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+-- ================================== Task table 
+
+CREATE TABLE tasks (
+    id INT NOT NULL AUTO_INCREMENT,
+    description VARCHAR(512),
+    task_category INT,
+    FOREIGN KEY (category)
+        REFERENCES categories (id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    dead_line DATETIME,
+    parent_id INT DEFAULT NULL,
+	FOREIGN KEY (parent_id)
+		REFERENCES tasks(id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    created_by INT,
+    FOREIGN KEY (created_by)
+        REFERENCES users (id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
+
+
+-- ================================== Mixed tables 
+
+CREATE TABLE project_users (
+	project_name INT NOT NULL, 
+    user_name INT NOT NULL,
+    FOREIGN KEY fk_project(project_name)
+		REFERENCES projects(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	FOREIGN KEY fk_user(user_name)
+		REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE tasks_responsibles (
+	task INT NOT NULL, 
+    responsible INT NOT NULL,
+    FOREIGN KEY fk_task(task)
+		REFERENCES tasks(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	FOREIGN KEY fk_responsible(responsible)
+		REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE chat_mess_tasks (
+	task INT NOT NULL, 
+    chat_message INT NOT NULL,
+    FOREIGN KEY fk_task(task)
+		REFERENCES tasks(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	FOREIGN KEY fk_chat_message(chat_message)
+		REFERENCES chat_messages(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE mail_mess_tasks (
+	task INT NOT NULL, 
+    mail_message INT NOT NULL,
+    FOREIGN KEY fk_task(task)
+		REFERENCES tasks(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	FOREIGN KEY fk_mail_message(mail_message)
+		REFERENCES mail_messages(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
