12e665e54b710fb3cdc4ec3a96ce46b18b89bf48
ManuCas <manucastle@hotmail.com>
2019-06-16 20:33:16 -0500

    second try push to test in heroku

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var session = require("express-session");
 var db = require("./models");
-
+var passport = require("./config/passport");
 var app = express();
+
 var PORT = process.env.PORT || 3000;
+var db = require("./models");
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
@@ -34,7 +41,7 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
